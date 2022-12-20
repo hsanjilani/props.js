462db511c42f2442f5958755cc065438d97585a8
hsanjilani <hssanjilani@gmail.com>
2022-12-20 22:48:04 +0100

    second commit

--- props/src/profile/profile.js
+++ props/src/profile/profile.js
@@ -3,7 +3,7 @@ import React from 'react';
 function Profile(){
     return(
         <div>
-            <img scr="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdLNikDy_OZSWYP3b59vcC1wDIOKa1rH_sOg0bXZNz6dr8U8A1d1ib9TeOUgN7jLG55Lc&usqp=CAU"alt='nice work'/>
+            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQdLNikDy_OZSWYP3b59vcC1wDIOKa1rH_sOg0bXZNz6dr8U8A1d1ib9TeOUgN7jLG55Lc&usqp=CAU"alt='nice work'/>
         </div>
     )
 }
